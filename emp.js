487f9e42f7f63537c7dbf6ba4e085208bacb166e
naga7557 <reach2nagaraj@gmail.com>
2023-12-15 10:39:35 +0530

    Add name field validation

--- emp.js
+++ emp.js
@@ -5,6 +5,13 @@ document.getElementById('employeeForm').addEventListener('submit', function(even
     var email = document.getElementById('email').value;
     var position = document.getElementById('position').value;
 
+    // Validation to accept only alphabets, dots, and spaces for the name field
+    var regex = /^[A-Za-z .]+$/;
+    if (!regex.test(name)) {
+        alert('Please enter a valid name with alphabets, dots, and spaces only.');
+        return;
+    }
+
     var result = document.getElementById('result');
     result.innerHTML = `<p>Name: ${name}</p><p>Email: ${email}</p><p>Position: ${position}</p>`;
 });
